e1081cad2664499139c2dd55a8ea8a59a2856370
Allan Li <allan0499@gmail.com>
2025-03-19 21:04:07 +0200

    chore: add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config'
-
+import tailwindcss from '@tailwindcss/vite'
 import vercel from '@astrojs/vercel/serverless'
-
-import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,4 +16,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 })
